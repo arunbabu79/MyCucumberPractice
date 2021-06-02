8c1055e37d0dfa516466a02e0dc5a9daf1ef8ac6
akannupa <akannupa@HEN018448.am.elcompanies.net>
2021-06-02 11:47:58 -0400

    Updated feature path

--- fb.cucumber/src/test/java/parallel/TestRunner.java
+++ fb.cucumber/src/test/java/parallel/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "C:\\Users\\akannupa\\eclipse-workspace\\fb.cucumber\\src\\test\\resources\\parallel", //path of feature file
+		features = "C:\\Users\\akannupa\\git\\MyCucumberPractice1\\fb.cucumber\\src\\test\\resources\\parallel", //path of feature file
 		glue = {"parallel"}, //path of step definition,
 		plugin = {"html:Report/cucumber-html-report.html", 
 				   "json:Report/cucumber-json-report.json","junit:Report/cucumber-junit-report.xml",
